447a782a2fce900a88b5ded95c4e6df960db91d7
diegofranc <100852246+diegofranc@users.noreply.github.com>
2022-06-22 17:02:00 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const app = express();
 
 const path = require ('path');
 
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || 'localhost';
+
 const publicPath = path.resolve(__dirname, './public');
 app.use( express.static(publicPath));
 
-app.listen (3000, ()=> console.log('Servidor puerto 3000 funcionando'));
+app.listen (PORT, ()=> console.log('Server running at http://${HOST}:${PORT}/'));
 
 app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname,'./views/home.html'));
